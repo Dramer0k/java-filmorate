4609754c331b8aeeea99641218834fde3842fa3d
MrDramer <dramerrt@gmail.com>
2026-05-09 22:11:10 +0300

    Fix checkStyle 1.8

--- src/test/java/ru/yandex/practicum/filmorate/dbtest/UserDbTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dbtest/UserDbTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dbtest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
